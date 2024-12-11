460bd0ba490337ecc8361d20d04cec5f12882587
Alexey Zhestkov <alezhestkov@gmail.com>
2024-12-11 17:32:43 +0400

    g

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-       new MatrixServer(7777);
-        //new ParallelMatrixServer(7777);
+        //new MatrixServer(7777);
+        new ParallelMatrixServer(7777);
     }
 }
